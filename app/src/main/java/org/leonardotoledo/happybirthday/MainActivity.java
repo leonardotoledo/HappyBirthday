826c7565470563502331e1e31711d45f61910ced
Leonardo Tolêdo <odelotodranoel@gmail.com>
2018-06-09 18:28:18 -0300

    First commit

---
+++ app/src/main/java/org/leonardotoledo/happybirthday/MainActivity.java
@@ -0,0 +1,15 @@
+package org.leonardotoledo.happybirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
